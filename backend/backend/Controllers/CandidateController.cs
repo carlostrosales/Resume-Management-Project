cbca203c21fe455cd3a8a051e89f7007ac6950bc
TheCarlosTovias <rosales.carlost@gmail.com>
2023-12-24 20:46:24 -0600

    Completed backend portion with candidate APIs

---
+++ backend/backend/Controllers/CandidateController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using backend.Core.Context;
+using backend.Core.Dtos.Candidate;
+using backend.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class CandidateController : Controller
+    {
+        private ApplicationDbContext _context { get;  }
+        private IMapper _mapper { get; }
+
+        public CandidateController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // CRUD
+
+        // Create
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateCandidate([FromForm] CandidateCreateDto dto, IFormFile pdfFile)
+        {
+            // First => Save pdf to Server
+            // Then => Save url into our entity
+            var fiveMegaByte = 5 * 1024 * 1024;
+            var pdfMimeType = "application/pdf";
+            if (pdfFile.Length > fiveMegaByte || pdfFile.ContentType != pdfMimeType)
+            {
+                return BadRequest("File is not valid");
+            }
+
+            var resumeUrl = Guid.NewGuid().ToString() + ".pdf";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "documents", "pdfs", resumeUrl);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await pdfFile.CopyToAsync(stream);
+            }
+            var newCandidate = _mapper.Map<Candidate>(dto);
+            newCandidate.ResumeUrl = resumeUrl;
+            await _context.Candidates.AddAsync(newCandidate);
+            await _context.SaveChangesAsync();
+
+            return Ok("Candidate Saved Successfully");
+        }
+
+        // Read
+        [HttpGet]
+        [Route("Get")]
+        public async Task<ActionResult<IEnumerable<CandidateGetDto>>> GetCandidate()
+        {
+            var candidates = await _context.Candidates.Include(c => c.Job).ToListAsync();
+            var convertedCandidates = _mapper.Map<IEnumerable<CandidateGetDto>>(candidates);
+
+            return Ok(convertedCandidates);
+        }
+
+        // Read (Download Pdf File)
+        [HttpGet]
+        [Route("download/{url}")]
+        public IActionResult DownloadPdfFile(string url)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "documents", "pdfs", url);
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("File Not Found");
+            }
+
+            var pdfBytes = System.IO.File.ReadAllBytes(filePath);
+            var file = File(pdfBytes, "application/pdf", url);
+            return file;
+
+        }
+        // Read (Get Candidate by ID)
+
+        // Update
+
+        // Delete
+
+    }
+}
+
